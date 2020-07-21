575389792e516d7bb1e6c39851b38537e987071d
BurNIng <361135947@qq.com>
2020-07-21 13:39:46 +0800

    s2项目

--- src/com/sfc/dao/OrderDetailedDao.java
+++ src/com/sfc/dao/OrderDetailedDao.java
@@ -2,27 +2,29 @@ package com.sfc.dao;
 
 import com.sfc.entity.OrderDetailed;
 
+import java.sql.SQLException;
+
 public interface OrderDetailedDao {
     /**
      * 根据订单编号获取订单信息
      * @param orderNo
      * @return
      */
-    public OrderDetailed getOrderDetailedByOrderNo(String orderNo);
+    public OrderDetailed getOrderDetailedByOrderNo(String orderNo) throws SQLException;
 
     /**
      * 添加一条订单
      * @param od
      * @return
      */
-    public int insertOrderDetailed(OrderDetailed od);
+    public int insertOrderDetailed(OrderDetailed od) throws SQLException;
 
     /**
      * 删除或取消订单
      * @param id
      * @return
      */
-    public int delOrderDetailed(int id);
+    public int delOrderDetailed(int id) throws SQLException;
 
 
 
--- src/com/sfc/impl/OrdereDetailedDaoImpl.java
+++ src/com/sfc/impl/OrdereDetailedDaoImpl.java
@@ -5,6 +5,8 @@ import com.sfc.dao.OrderDetailedDao;
 import com.sfc.entity.OrderDetailed;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class OrdereDetailedDaoImpl extends BaseDao implements OrderDetailedDao {
     public OrdereDetailedDaoImpl(Connection conn) {
@@ -12,17 +14,40 @@ public class OrdereDetailedDaoImpl extends BaseDao implements OrderDetailedDao {
     }
 
     @Override
-    public OrderDetailed getOrderDetailedByOrderNo(String orderNo) {
-        return null;
+    public OrderDetailed getOrderDetailedByOrderNo(String orderNo) throws SQLException{
+        String strSql = "select * from OrderDetailed where oid = ?";
+        Object[] param = {orderNo};
+        List<OrderDetailed> list = null;
+        try {
+            list = this.getEntity(new OrderDetailed(), param, strSql);
+            if(list.size() > 0) {
+                return list.get(0);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
-    public int insertOrderDetailed(OrderDetailed od) {
-        return 0;
+    public int insertOrderDetailed(OrderDetailed od) throws SQLException{
+        String strSql = "insert into OrderDetailed(orderNo,oCount,bid,oPrice,oBookName) values(?,?,?,?,?)";
+        Object[] param = {od.getoRderNo(),od.getoCount(),od.getbId(),od.getoPrice(),od.getoBookName()};
+        try {
+            return this.executeUpdate(strSql,param);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
-    public int delOrderDetailed(int id) {
-        return 0;
+    public int delOrderDetailed(int id) throws SQLException{
+        String strSql = "delete from OrderDetailed where oid = ?";
+        Object[] param = {id};
+        try {
+            return this.executeUpdate(strSql,param);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 }
