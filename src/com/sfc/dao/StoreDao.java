c1bcb65835d828bdeac5405578dc6e5b6e9df353
BurNIng <361135947@qq.com>
2020-07-21 09:59:01 +0800

    s2项目

--- src/com/sfc/dao/StoreDao.java
+++ src/com/sfc/dao/StoreDao.java
@@ -2,6 +2,7 @@ package com.sfc.dao;
 
 import com.sfc.entity.Store;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface StoreDao {
@@ -10,21 +11,21 @@ public interface StoreDao {
      * @param uId
      * @return
      */
-    public List<Store> getStoreByUId(int uId);
+    public List<Store> getStoreByUId(int uId) throws SQLException;
 
     /**
      * 添加到收藏夹
      * @param st
      * @return
      */
-    public int insertStore(Store st);
+    public int insertStore(Store st) throws SQLException;
 
     /**
      * 根据图书id移除图书于收藏夹
      * @param uId
      * @return
      */
-    public int delStoreBybId(int uId);
+    public int delStoreByuId(int uId) throws SQLException;
 
 
 
