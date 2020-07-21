a03152ce85bf067c611fc6cdcede32605e91c307
BurNIng <361135947@qq.com>
2020-07-21 10:29:46 +0800

    s2项目

--- src/com/sfc/dao/ShoppingCartDao.java
+++ src/com/sfc/dao/ShoppingCartDao.java
@@ -2,6 +2,7 @@ package com.sfc.dao;
 
 import com.sfc.entity.ShoppingCart;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ShoppingCartDao {
@@ -10,28 +11,28 @@ public interface ShoppingCartDao {
      * @param uId
      * @return
      */
-    public List<ShoppingCart> getShoppingCartByUId(int uId);
+    public List<ShoppingCart> getShoppingCartByUId(int uId) throws SQLException;
 
     /**
      * 添加商品到购物车
      * @param sc
      * @return
      */
-    public int insertShoppingCart(ShoppingCart sc);
+    public int insertShoppingCart(ShoppingCart sc) throws SQLException;
 
     /**
      * 更新购物车商品数量
      * @param sc
      * @return
      */
-    public int updateShoppingCartCount(ShoppingCart sc);
+    public int updateShoppingCartCountByuId(ShoppingCart sc) throws SQLException;
 
     /**
      * 根据购物车id删除购物车信息
      * @param shId
      * @return
      */
-    public int delShoppingCartByShId(int shId);
+    public int delShoppingCartByShId(int shId) throws SQLException;
 
     /**
      * 获取每页信息
@@ -39,7 +40,7 @@ public interface ShoppingCartDao {
      * @param curtotalSize
      * @return
      */
-    public List<ShoppingCart> getShoppingCartPage(int currentPage,int curtotalSize);
+    public List<ShoppingCart> getShoppingCartPage(int currentPage,int curtotalSize) throws SQLException;
 
 
 }
--- src/com/sfc/impl/ShoppingCartDaoImpl.java
+++ src/com/sfc/impl/ShoppingCartDaoImpl.java
@@ -5,6 +5,7 @@ import com.sfc.dao.ShoppingCartDao;
 import com.sfc.entity.ShoppingCart;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
@@ -13,23 +14,47 @@ public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
     }
 
     @Override
-    public List<ShoppingCart> getShoppingCartByUId(int uId) {
-        return null;
+    public List<ShoppingCart> getShoppingCartByUId(int uId) throws SQLException{
+        String strSql = "select * from shoppingcart where uid = ?";
+        Object[] param = {uId};
+        try {
+            return this.getEntity(new ShoppingCart(),param,strSql);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
-    public int insertShoppingCart(ShoppingCart sc) {
-        return 0;
+    public int insertShoppingCart(ShoppingCart sc) throws SQLException{
+        String strSql = "insert info ShoppingCart(bid,shDate,uid,shCount) values(?,?,?,?)";
+        Object[] param = {sc.getbId(),sc.getShDate(),sc.getuId(),sc.getShCount()};
+        try {
+            return this.executeUpdate(strSql,param);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
-    public int updateShoppingCartCount(ShoppingCart sc) {
-        return 0;
+    public int updateShoppingCartCountByuId(ShoppingCart sc) throws SQLException{
+        String strSql = "update ShoppingCart set shCount = ? where uid = ?";
+        Object[] param = {sc.getShCount(),sc.getuId()};
+        try {
+            return this.executeUpdate(strSql,param);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
-    public int delShoppingCartByShId(int shId) {
-        return 0;
+    public int delShoppingCartByShId(int shId) throws SQLException{
+        String strSql = "delete from ShoppingCart where shId = ?";
+        Object[] param = {shId};
+        try {
+            return this.executeUpdate(strSql,param);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
