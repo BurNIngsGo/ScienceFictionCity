24666b5ca22c7499e4a9163dd560d166942db620
BurNIng <361135947@qq.com>
2020-07-21 10:32:03 +0800

    s2项目

--- src/com/sfc/impl/ShoppingCartDaoImpl.java
+++ src/com/sfc/impl/ShoppingCartDaoImpl.java
@@ -13,7 +13,11 @@ public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
         super(conn);
     }
 
-    @Override
+    /**
+     * 根据用户id获取该用户的购物车信息
+     * @param uId
+     * @return
+     */
     public List<ShoppingCart> getShoppingCartByUId(int uId) throws SQLException{
         String strSql = "select * from shoppingcart where uid = ?";
         Object[] param = {uId};
@@ -24,7 +28,11 @@ public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
         }
     }
 
-    @Override
+    /**
+     * 添加商品到购物车
+     * @param sc
+     * @return
+     */
     public int insertShoppingCart(ShoppingCart sc) throws SQLException{
         String strSql = "insert info ShoppingCart(bid,shDate,uid,shCount) values(?,?,?,?)";
         Object[] param = {sc.getbId(),sc.getShDate(),sc.getuId(),sc.getShCount()};
@@ -35,7 +43,11 @@ public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
         }
     }
 
-    @Override
+    /**
+     * 更新购物车商品数量
+     * @param sc
+     * @return
+     */
     public int updateShoppingCartCountByuId(ShoppingCart sc) throws SQLException{
         String strSql = "update ShoppingCart set shCount = ? where uid = ?";
         Object[] param = {sc.getShCount(),sc.getuId()};
@@ -46,7 +58,11 @@ public class ShoppingCartDaoImpl extends BaseDao implements ShoppingCartDao {
         }
     }
 
-    @Override
+    /**
+     * 根据购物车id删除购物车信息
+     * @param shId
+     * @return
+     */
     public int delShoppingCartByShId(int shId) throws SQLException{
         String strSql = "delete from ShoppingCart where shId = ?";
         Object[] param = {shId};
